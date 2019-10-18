903f989d2f39fabcd4dfdd42e61e4ab2c9ecb017
Svetlin Kozhuharov <kozhuhar@progress.com>
2019-10-18 12:41:00 +0300

    upload project using Core 3

---
+++ Controllers/StreamingController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace upload.Controllers
+{
+
+    public class StreamingController : Controller
+    {
+
+        private readonly IWebHostEnvironment _webHhostingEnvironment;
+
+        public StreamingController(IWebHostEnvironment webHostingEnvironment)
+        {
+            _webHhostingEnvironment = webHostingEnvironment;
+        }
+
+        [Route("api/Upload")]
+        [HttpPost]
+        public async Task<IActionResult> OnPostUploadAsync(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.GetTempFileName();
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // Process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size });
+        }
+
+        [Route("api/Remove")]
+        [HttpPost]
+        public ActionResult Async_Remove(string[] fileNames)
+        {
+
+            if (fileNames != null)
+            {
+                foreach (var fullName in fileNames)
+                {
+                    var fileName = Path.GetFileName(fullName);
+                    var physicalPath = Path.Combine(_webHhostingEnvironment.WebRootPath, "App_Data", fileName);
+
+                    // TODO: Verify user permissions
+
+                    if (System.IO.File.Exists(physicalPath))
+                    {
+                        // The files are not actually removed in this demo
+                        // System.IO.File.Delete(physicalPath);
+                    }
+                }
+            }
+
+            // Return an empty string to signify success
+            return Content("");
+        }
+    }
+}
+
